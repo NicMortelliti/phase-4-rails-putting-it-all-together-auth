929e3e75016d6366d0c07c5958e40411fe480b15
NicMortelliti <npmortelliti@gmail.com>
2022-08-15 12:09:06 -0700

    add validation for title and instructions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
   has_secure_password
+  validates :title, presence: true
+
+  # Instructions must be present and at least 50 chars
+  validates :instructions, presence: true, length: { minimum: 50 }
 end
