49c6e2f35a45ca7ee427207c249a25f9003251aa
NicMortelliti <npmortelliti@gmail.com>
2022-08-15 12:02:47 -0700

    Add base recipe resource

---
+++ db/migrate/20220815190207_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :instructions
+      t.string :text
+      t.string :minutes_to_complete
+      t.string :integer
+
+      t.timestamps
+    end
+  end
+end
