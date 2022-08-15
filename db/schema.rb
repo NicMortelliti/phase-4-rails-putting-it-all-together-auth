ca7f648770116393b3b43d03dd2d3c7239b1f6be
NicMortelliti <npmortelliti@gmail.com>
2022-08-15 12:09:47 -0700

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_15_184241) do
+ActiveRecord::Schema.define(version: 2022_08_15_190207) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.string "instructions"
+    t.string "text"
+    t.string "minutes_to_complete"
+    t.string "integer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
