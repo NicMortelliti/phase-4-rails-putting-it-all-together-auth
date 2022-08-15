24232108c746871c78fcb71c6ef461127544a6f0
NicMortelliti <npmortelliti@gmail.com>
2022-08-15 16:16:41 -0700

    set up route to sessions login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
 
   post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
 
   get '/me', to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
